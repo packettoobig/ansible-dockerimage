edaa07e9c82b21628f7d5cfc95ac981448d6ec9d
packettoobig <53047591+packettoobig@users.noreply.github.com>
2021-02-09 17:59:10 +0100

    Adding auth sock mount functionnality

--- docker.sh
+++ docker.sh
@@ -5,6 +5,7 @@
 #
 randomstring=$(cat /dev/urandom | base64 | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 sleeptime=0
+mntbaseauthsock='/ssh-auth-sock'
 mntsrc1="$HOME/.ssh/id_rsa"
 mntdst1="/root/.ssh/id_rsa"
 mntopt1="ro"
@@ -18,6 +19,20 @@ containerpath="packettoobig/$container:$containerversion"
 containerhostname="container-$container-$randomstring"
 
 #
+# INIT
+#
+unameresult="$(uname -s)"
+# Mac has a specific way of handling the ssh auth sock
+if [[ "$unameresult" == "Darwin" ]]
+then
+    local_ssh_authsock='/run/host-services/ssh-auth.sock'
+    authsockmnt="$mntbaseauthsock"
+else
+    local_ssh_authsock=$(dirname $SSH_AUTH_SOCK)
+    authsockmnt="$mntbaseauthsock/ssh"
+fi
+
+#
 # SCRIPT
 #
 echo "####################################################################################################"
@@ -26,6 +41,8 @@ echo "##########################################################################
 sleep $sleeptime
 echo "Lauching $containerhostname"
 sudo docker run --name $containerfullname -h $containerhostname \
+	-v $local_ssh_authsock:$mntbaseauthsock \
+	-e SSH_AUTH_SOCK=$authsockmnt \
 	-v $mntsrc1:$mntdst1:$mntopt1 \
 	-v $mntsrc2:$mntdst2:$mntopt2 \
 	-it $containerpath /bin/bash
